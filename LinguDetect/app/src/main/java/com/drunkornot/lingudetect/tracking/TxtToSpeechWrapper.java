0fe17e2297d86f2c77a544b9f51a288c0a0e889f
Dominika <ddd.wilk@gmail.com>
2020-03-26 22:02:46 +0100

    Added codebase which is basically object_detection example project
    We will work on it later

---
+++ LinguDetect/app/src/main/java/com/drunkornot/lingudetect/tracking/TxtToSpeechWrapper.java
@@ -0,0 +1,8 @@
+package org.tensorflow.lite.examples.detection.tracking;
+
+import android.speech.tts.TextToSpeech;
+
+public class TxtToSpeechWrapper {
+    public static TextToSpeech instance;
+
+}
