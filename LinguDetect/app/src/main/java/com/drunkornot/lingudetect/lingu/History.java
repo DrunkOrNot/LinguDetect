3cc2a454e45b9d6caf6a9fb9b6f6e4b5f2092113
Agnieszka <agncwi@gmail.com>
2020-04-23 21:50:17 +0200

    WIP RecyclerView in HistoryActivity

--- LinguDetect/app/src/main/java/com/drunkornot/lingudetect/lingu/History.java
+++ LinguDetect/app/src/main/java/com/drunkornot/lingudetect/lingu/History.java
@@ -1,13 +1,34 @@
 package com.drunkornot.lingudetect.lingu;
 
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
 import java.util.ArrayList;
 
-public class History {
+public class History extends RecyclerView.Adapter {
 
     ArrayList<Result> history = new ArrayList<>();
 
     public History() {
 
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
     }
 
     public void Add(Result result) {
